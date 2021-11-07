837f43bea6488f896c4c01ad8aa0874e2eef12f9
Edson Sales <edsonsales99@gmail.com>
2021-11-06 23:35:31 -0500

    Criando entidades, order, OrderStatus é Product

--- delivery-java-jdbc/src/app/Program.java
+++ delivery-java-jdbc/src/app/Program.java
@@ -6,6 +6,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Order;
+import entities.OrderStatus;
+import entities.Product;
 
 public class Program {
 
@@ -15,10 +18,37 @@ public class Program {
 	
 		Statement st = conn.createStatement();
 			
-		ResultSet rs = st.executeQuery("select * from tb_product");
+		ResultSet rs = st.executeQuery("select * from tb_order");
 			
 		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+			
+
+			Order order = instantiateOrder(rs);
+			
+			System.out.println(order);
+			//System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
 		}
+	}
+	private static Order instantiateOrder(ResultSet rs) throws SQLException {
+		Order order = new Order();
+		order.setId(rs.getLong("id"));
+		order.setLatitude(rs.getDouble("latitude"));
+		order.setLongitude(rs.getDouble("longitude"));
+		order.setMoment(rs.getTimestamp("moment").toInstant());
+		order.setStatus(OrderStatus.values()[rs.getInt("status")]);
+		
+		return order;
+	}
+	
+	//Método que retorna uma instancia da classe produto
+	private static Product instantiateProduct(ResultSet rs) throws SQLException {
+
+		Product p = new Product();
+		p.setId(rs.getLong("Id"));
+		p.setDescription(rs.getString("description"));
+		p.setName(rs.getString("name"));
+		p.setImageUri(rs.getString("image_uri"));;
+		p.setPrice(rs.getDouble("price"));
+		return p;
 	}
 }
